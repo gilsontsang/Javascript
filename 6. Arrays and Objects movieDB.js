9d003e46be449209976ca322ccb87eaab2b27115
Gilson Tsang <37494996+gilsontsang@users.noreply.github.com>
2018-05-02 14:07:43 -0700

    Add files via upload

---
+++ 6. Arrays and Objects movieDB.js
@@ -0,0 +1,33 @@
+// Objects nested inside Arrays
+
+var movieDB = [
+	{name: "In Bruges",
+	rating: 5,
+	hasWatched: true
+	},
+	{name: "Frozen",
+	rating: 4.5,
+	hasWatched: false
+	},
+	{name: "Mad Max Fury Road",
+	rating: 5,
+	hasWatched: true
+	},
+	{name: "Les Miserables",
+	rating: 3.5,
+	hasWatched: false
+	}
+]
+
+
+// Output to user
+
+
+for (var i=0; i<movieDB.length; i++) {
+	if (movieDB[i].hasWatched === true) {
+		console.log("You have watched \"" + movieDB[i].name + "\" - " + movieDB[i].rating + " stars"); 
+	}
+	else {
+		console.log("You have not seen \"" + movieDB[i].name + "\" - " + movieDB[i].rating + " stars"); 
+	}
+}
